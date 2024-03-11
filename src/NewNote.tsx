73cb9f6fccd2a72e5bfd421d15a0f5dfd937c180
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-03-11 22:34:39 +0200

    start drafting

---
+++ src/NewNote.tsx
@@ -0,0 +1,12 @@
+import { NoteData, Tag } from "./App"
+import { NoteForm } from "./NoteForm"
+
+
+
+export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
+  return (
+    <>
+
+    </>
+  )
+}
