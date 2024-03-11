c702079d5476160ce896377c231e42a4f9845fd4
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-03-11 22:44:44 +0200

    note form

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,12 +1,11 @@
-import { NoteData, Tag } from "./App"
-import { NoteForm } from "./NoteForm"
-
-
+import { NoteData, Tag } from "./App";
+import { NoteForm } from "./NoteForm";
 
 export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
   return (
     <>
-
+      <h1 className="mb-4">New note</h1>
+      <NoteForm/>
     </>
-  )
+  );
 }
