507a51c640cfd3b42bc0abbe266b246c2e239e20
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-03-13 22:20:51 +0200

    starting notelist

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react"
+import { useMemo, useState } from "react";
 import {
   Badge,
   Button,
@@ -8,9 +8,39 @@ import {
   Modal,
   Row,
   Stack,
-} from "react-bootstrap"
-import { Link } from "react-router-dom"
-import ReactSelect from "react-select"
-import { Tag } from "./App"
-import styles from "./NoteList.module.css"
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ReactSelect from "react-select";
+import { Tag } from "./App";
+import styles from "./NoteList.module.css";
 
+export function NoteList() {
+  return (
+    <>
+      <Row>
+        <Col>
+          {" "}
+          <h1>Notes</h1>
+        </Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to="/new">
+              <Button variant="primary">Create</Button>
+            </Link>
+            <Button variant="outline-secondary">Edit Tags</Button>
+          </Stack>
+        </Col>
+      </Row>
+      <Form>
+        <Row className="mb-4">
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text"></Form.Control>
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+}
